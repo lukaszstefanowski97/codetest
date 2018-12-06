1ba303869b908b6e6f03cd28ea3f615ba684b94f
Lukasz Stefanowski <lukasz.stefanowski@avid.com>
2018-12-06 20:05:22 +0100

    service implementation

--- blog-web/src/main/java/com/pierceecom/blog/Service.java
+++ blog-web/src/main/java/com/pierceecom/blog/Service.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 @org.springframework.stereotype.Service
 public class Service {
 
-    static Repository repository;
+    public static Repository repository;
 
     public static Boolean doesPostExist(String id) {
         return repository.existsById(id);
@@ -21,11 +21,19 @@ public class Service {
         }
     }
 
+    public static void updatePost(PostDto post) throws Exceptions.PostExistsException {
+        if (doesPostExist(post.id)) {
+            repository.save(post);
+        } else {
+            throw new Exceptions.PostExistsException("404 Post with given id does not exist");
+        }
+    }
+
     public void deletePost(String id) throws Exceptions.PostExistsException {
         if (doesPostExist(id)) {
             repository.deleteById(id);
         } else {
-            throw new Exceptions.PostExistsException("Post with given id does not exist");
+            throw new Exceptions.PostExistsException("404 Post with given id does not exist");
         }
     }
 
@@ -33,7 +41,10 @@ public class Service {
         repository.findAll();
     }
 
-    public Optional<PostDto> findPostWithGivenId(String id) {
+    public Optional<PostDto> findPostWithGivenId(String id) throws Exceptions.PostExistsException {
+        if (!doesPostExist(id)) {
+            throw new Exceptions.PostExistsException("404 Post with given id does not exist");
+        }
         return repository.findById(id);
     }
 }
